999c32d91f07fa738b19f70000e2a502664c7e40
jctaoo <jctaoo@outlook.com>
2025-08-25 18:52:28 +0800

    chore: update package.json and pnpm-lock.yaml to include Vitest coverage support and upgrade dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    pool: "vmForks"
+    pool: "vmForks",
+    coverage: {
+      enabled: true,
+      provider: "v8",
+    }
   },
 })
